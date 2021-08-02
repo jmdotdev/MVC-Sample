22091aa48284d42c7ab34c8fa156c93504caee88
Jollymar Obias <jollymarobias25@gmail.com>
2021-08-02 22:40:37 +0800

    first commit

---
+++ MVCProject/Controllers/TrainingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Controllers
+{
+    public class TrainingController : Controller
+    {
+        private readonly ApplicationDBContext dBContext;
+        public TrainingController(ApplicationDBContext context)
+        {
+            this.dBContext = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<TrainingTable> result = dBContext.TRAININGS.OrderBy(x => x.TRAINING_NAME).ToList();
+
+            return View(result);
+        }
+    }
+}
---
+++ MVCProject/Data/TrainingTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Data
+{
+    public class TrainingTable
+    {
+
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TRAINING_NAME { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string DESCRIPTION { get; set; }
+
+        public bool  STATUS { get; set; }
+    }
+}
